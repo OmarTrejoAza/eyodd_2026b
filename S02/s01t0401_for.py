2f60d2f017b30ed91de5f577b87419fee79f8b42
OmarTrejoAza <trejoazaomar@gmail.com>
2026-09-24 10:56:26 -0600

    ⭐ s01t0401 for

--- S02/s01t0401_for.py
+++ S02/s01t0401_for.py
@@ -8,49 +8,69 @@ calculara la suma del 1 al 100
 #Importarmos biblioteca time
 import time
 
-# Tomando el timempo inicial
-timestamp_01 = time.time ()
-
+"""
 #Programa que calcula las sumas
 # de los "n" numeros naturales
 
 n = 4000
 total_sum = 0
 
+
+
+
 # Ciclo for
 for number in range(1, n+1):
     print(number, end=",")
 print ("")
 
-print ("")
-print ("")
-print ("")
+"""
 
-# Ciclo for pero en suma 
-for number1 in range(1, n+1):
-    total_sum = total_sum + number1
-    """
-    1: sum <- 0 + 1
-    sum = 1
-    2: sum <- 1 + 2
-    sum = 3
-    3: sum <- 3 + 3
-    sum = 6
-    ...
-    100: sum <- antSum + 100
-    """
-print (f"La suma de 1 hasta {n} es: {total_sum}")
-# f = formal formato, f string 
+#Funcion que suma los 
+#primeros "n" numeros naturales
+def sum_of_n(n):
+    total_sum = 0
+    #sumano los "n" numeros
+    # Ciclo for pero en suma 
+    for number1 in range(1, n+1):
+        total_sum = total_sum + number1
+        """
+        1: sum <- 0 + 1
+        sum = 1
+        2: sum <- 1 + 2
+        sum = 3
+        3: sum <- 3 + 3
+        sum = 6
+        ...
+        100: sum <- antSum + 100
+        """
+    #Retornando el toal de la suma 
+    return total_sum
 
-print ("")
-print ("")
-print ("")
+#Variable para guardar
+#El data set
+dataset = [] #[ (n,time,sum),(n,time,sum) ]
 
-#Tomando el timepo final 
-timestamp_02 = time.time ()
+#Genreando el contenido del Datset
+for repiticion in range(1,11):
+    #⏱️Tomo el tiempo 1 (INICIAL)
+    timestamp_01 = time.time ()
+    #sumo los "n" numeros 
+    n = repiticion*500
+    result = sum_of_n(n)
 
-#Impresion del tiempo de ejecuciom
-print(f"tiempo de ejecucion: {(timestamp_02 - timestamp_01) * 1e6:.2f} µs")
+    #⏱️Tomando el timepo final 
+    timestamp_02 = time.time ()
+
+    #Calculando el tiempo
+    elapsed_time=  round (( timestamp_02 - timestamp_01) * 1e6,2)
+
+    #Agregar la tripleta de los
+    #datos al dataset
+    dataset.append((n,elapsed_time,result))
+
+#Impriir el dataset
+for tup in dataset:
+    print (tup)
 
 """
   n  |   µ
@@ -66,3 +86,4 @@ print(f"tiempo de ejecucion: {(timestamp_02 - timestamp_01) * 1e6:.2f} µs")
 4000 | 8218.77 µs
 
 """
+
